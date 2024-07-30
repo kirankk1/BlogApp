d0827b655aac85ee1a0cda7860488e9dce515dd2
= <porlakiran@gmail.com>
2024-07-30 23:38:32 +0530

    added pages and created routes

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element = {<About />} />
+        <Route path="/dashboard" element = {<Dashboard />}/>
+        <Route path="/projects" element= {<Projects/>} />
+        <Route path="/sign-in" element= {<SignIn/>} />
+        <Route path="/sign-up" element = {<SignUp/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
