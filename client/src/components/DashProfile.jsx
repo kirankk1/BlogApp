091e1141b7cac83c1efe5030fa78e77598fd7d08
= <porlakiran@gmail.com>
2024-09-18 16:12:17 +0530

    Complete update Profile Page Functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -18,6 +18,9 @@ export default function DashProfile() {
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
   const [imageFileUploadError, setImageFileError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
   const [formData, setFormData] = useState({})
   const filePickerRef = useRef();
   const dispatch = useDispatch()
@@ -38,6 +41,7 @@ export default function DashProfile() {
   }, [imageFile]);
 
   const uploadImage = async () => {
+    setImageFileUploading(true);
     setImageFileError(null)
     const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile.name;
@@ -57,11 +61,13 @@ export default function DashProfile() {
         setImageFileUploadProgress(null)
         setImageFile(null)
         setImageFileUrl(null)
+        setImageFileUploading(false)
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURl) => {
           setImageFileUrl(downloadURl);
           setFormData({...formData, profilePicture:downloadURl});
+          setImageFileUploading(false)
         });
       }
     );
@@ -73,9 +79,17 @@ export default function DashProfile() {
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
+    setUpdateUserError(null)
+    setUpdateUserSuccess(null)
     if(Object.keys(formData).length === 0){
+      setUpdateUserError('No changes Made');
       return
-    }try{
+    }
+    if(imageFileUploading){
+      setUpdateUserError('Please wait for Image to upload');
+      return
+    }
+    try{
       dispatch(updateStart());
       const res = await fetch(`/api/user/update/${currentUser._id}`,{
         method: 'PUT',
@@ -84,16 +98,18 @@ export default function DashProfile() {
         },
         body: JSON.stringify(formData),
       });
-      console.log(formData);
       
       const data = await res.json()
       if(!res.ok){
         dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
       }else{
         dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User Profile Updated Successfully")
       }
     }catch(error){
       dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
     }
   }
   
@@ -167,6 +183,12 @@ export default function DashProfile() {
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className="mt-5">{updateUserSuccess}</Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className="mt-5">{updateUserError}</Alert>
+      )}
     </div>
   );
 }
