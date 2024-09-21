280cbff9f08653bc99be236144ca6f523f3172c6
= <porlakiran@gmail.com>
2024-09-21 23:01:15 +0530

    add show more button

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,6 +5,8 @@ import { Table, TableBody } from "flowbite-react";
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
+  const [showMore, setShowMore] = useState(true);
+
   const [userPosts, setUserPosts] = useState([]);
   console.log(userPosts);
 
@@ -15,6 +17,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +29,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async ()=>{
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json()
+      if(res.ok){
+        setUserPosts((prev)=>[...prev, ...data.posts]);
+        setShowMore(false);
+      }
+      
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
+
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -80,6 +103,11 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">Show More</button>
+            )
+          }
         </>
       ) : (
         <p>You have no posts yet! </p>
@@ -87,3 +115,6 @@ export default function DashPosts() {
     </div>
   );
 }
+
+
+//7:03:30
