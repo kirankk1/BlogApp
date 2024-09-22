9361284713474263217b102584785d630ba369de
= <porlakiran@gmail.com>
2024-09-22 14:27:56 +0530

    add update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 function App() {
   return (
@@ -25,6 +26,8 @@ function App() {
           </Route>
           <Route element={<OnlyAdminPrivateRoute/>}>
             <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:postId" element={<UpdatePost />} />
+
           </Route>
           <Route path="/projects" element={<Projects />} />
           <Route path="/sign-in" element={<SignIn />} />
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -171,5 +171,3 @@ export default function DashPosts() {
     </div>
   );
 }
-
-
