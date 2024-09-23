30ed946aaf991c4188740d06663fa80818b81bd1
= <porlakiran@gmail.com>
2024-09-23 22:45:45 +0530

    ''

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
@@ -30,8 +33,7 @@ function App() {
 
           </Route>
           <Route path="/projects" element={<Projects />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/post/:postSlug" element={<PostPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ client/src/pages/PostPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PostPage() {
+  return (
+    <div>
+      <h1>Post Page</h1>
+
+    </div>
+  )
+}
