9ee67f7e67f19283b86b457a4e27e5b944a31257
= <porlakiran@gmail.com>
2024-09-27 17:18:52 +0530

    show the comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,25 +1,37 @@
 import { errorHandler } from "../utils/error.js";
-import Comment from '../models/comment.model.js'
-export const createComment = async (req, res, next) =>{
-    try {
-        const {content, postId, userId} = req.body;
+import Comment from "../models/comment.model.js";
+export const createComment = async (req, res, next) => {
+  try {
+    const { content, postId, userId } = req.body;
 
-        if(userId !== req.user.id){
-            return next(errorHandler(403, 'You are not allowed to create this comment'));
-        }
-        if (typeof postId !== 'string') {
-            return next(errorHandler(400, 'Invalid postId format'));
-        }
-
-        const newComment = new Comment({
-            content,
-            postId,
-            userId,
-        });
-        await newComment.save();
-        res.status(200).json(newComment);
-
-    } catch (error) {
-        next(error)
+    if (userId !== req.user.id) {
+      return next(
+        errorHandler(403, "You are not allowed to create this comment")
+      );
     }
-}
+    if (typeof postId !== "string") {
+      return next(errorHandler(400, "Invalid postId format"));
+    }
+
+    const newComment = new Comment({
+      content,
+      postId,
+      userId,
+    });
+    await newComment.save();
+    res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
