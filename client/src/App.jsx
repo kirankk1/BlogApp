39d6fd319c88854dd6fa3611b4e37fb2999d0188
= <porlakiran@gmail.com>
2024-09-29 12:05:43 +0530

    added search page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/search" element={<Search />} />
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,8 +6,8 @@ import {
   Avatar,
   DropdownDivider,
 } from "flowbite-react";
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
@@ -20,6 +20,18 @@ export default function Header() {
   const path = useLocation().pathname;
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm ] = useState('')
+  const location = useLocation();
+  const navigate = useNavigate()
+  
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  },[location.search])
 
   const handleSignout = async () => {
     try {
@@ -37,6 +49,15 @@ export default function Header() {
     }
   };
 
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`)
+
+  }
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -48,12 +69,14 @@ export default function Header() {
         </span>
         Blog
       </Link>
-      <form>
+      <form  onSubmit={handleSubmit}>
         <TextInput
           placeholder="Search"
           type="text"
           rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
+          className="hidden lg:inline" 
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-10 h-9 lg:hidden" color="gray" pill>
@@ -76,7 +99,7 @@ export default function Header() {
               <Avatar
                 alt="User Avatar"
                 img={currentUser.profilePicture}
-                rounded // Fallback to default image
+                rounded 
               />
             }
           >
