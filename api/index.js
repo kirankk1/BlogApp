2af739608a3e2a2848e1c427c91a43bdf4977f76
= <porlakiran@gmail.com>
2024-09-29 16:47:27 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-import postRoutes from './routes/post.route.js'
-import commentRoutes from './routes/comment.route.js'
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -22,25 +22,28 @@ mongoose
     console.log(err);
   });
 
-  app.listen(3000, () => {
+  const  __dirname = path.resolve()
+
+app.listen(3000, () => {
   console.log("Server is running on port http://localhost:3000");
 });
 
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
-app.use('/api/user', userRoutes)
-
-app.use('/api/auth', authRoutes)
-app.use('/api/post', postRoutes)
-app.use('/api/comment', commentRoutes);
-
-
-
-app.use((err, req, res, next) =>{
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success:false,
-    statusCode,
-    message
-  })
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,6 @@ export default defineConfig({
         secure: false,
       },
     }
-  },
+  }, 
   plugins: [react()],
 })
